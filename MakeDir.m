1b43ba92af327b9d1b8db634b9f85c19e970b3e0
daraccrafter <96226312+daraccrafter@users.noreply.github.com>
2021-12-19 01:34:23 +0100

    Update MakeDir.m

--- MakeDir.m
+++ MakeDir.m
@@ -2,8 +2,8 @@ cards = {'2 of clubs', '2 of diamonds', '2 of hearts', '2 of spades', '3 of club
          '5 of clubs', '5 of diamonds', '5 of hearts', '5 of spades','6 of clubs', '6 of diamonds', '6 of hearts', '6 of spades','7 of clubs', '7 of diamonds', '7 of hearts', '7 of spades',...
          '8 of clubs', '8 of diamonds', '8 of hearts', '8 of spades','9 of clubs', '9 of diamonds', '9 of hearts', '9 of spades','10 of clubs', '10 of diamonds', '10 of hearts', '10 of spades',...
          'ace of clubs', 'ace of diamonds', 'ace of hearts', 'ace of spades','joker of clubs', 'joker of diamonds', 'joker of hearts', 'joker of spades','king of clubs', 'king of diamonds', 'king of hearts', 'king of spades',...
-         'queen of clubs', 'queen of diamonds', 'queen of hearts', 'queen of spades',};
-mkdir("Dataset");
-for i=1:length(cards)
-    mkdir("Dataset",""+cards(1,i))
+         'queen of clubs', 'queen of diamonds', 'queen of hearts', 'queen of spades',}; %setup array of all card names
+mkdir("Dataset");  %make folder "Dataset"
+for i=1:length(cards) %iterate through all the cards in array
+    mkdir("Dataset",""+cards(1,i)) %make subfolder of each cards name in the folder "Dataset"
 end
